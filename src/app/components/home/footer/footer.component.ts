1b0f949f2cf9362d63ebe8ecb633057b209e9d8e
avarghese <arun.varghese@masergy.com>
2024-03-25 06:26:28 -0500

    update styles

--- src/app/components/home/footer/footer.component.ts
+++ src/app/components/home/footer/footer.component.ts
@@ -5,22 +5,7 @@ import {DOCUMENT} from "@angular/common";
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
-  animations:[
-    trigger("animateFooter", [
-      transition(":enter", [
-        query("*", [
-          style({opacity: 0, transform: "translateY(100%)"}),
-          stagger(50, [
-            animate(
-              "250ms cubic-bezier(0.35, 0, 0.25, 1)",
-              style({opacity:1, transform: "none"})
-            )
-          ])
-        ])
-      ])
-    ])
-  ]
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
 }

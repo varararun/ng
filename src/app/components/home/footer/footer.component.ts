6f1a10971f147e4cbc4735508632e7683acf7f12
avarghese <arun.varghese@masergy.com>
2024-03-25 23:17:51 -0500

    scrolltop, projects

--- src/app/components/home/footer/footer.component.ts
+++ src/app/components/home/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
     selector: 'app-footer',
@@ -6,4 +6,19 @@ import {Component} from '@angular/core';
     styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+
+    scrollPosition = 0;
+
+    @HostListener('window:scroll')
+    checkScroll() {
+        this.scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    }
+
+    scrollTop() {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
 }
